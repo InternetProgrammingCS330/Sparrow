5094954f067a7bf331ac12b00ad749786851207a
sparrow <alevar@gmail.com>
2016-04-29 19:32:00 -0500

    sudo make me a sandwich

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, Blueprint
+from flask import Flask, request, Blueprint, render_template, \
+                  flash, g, session, redirect, url_for
 
 app = Flask(__name__)
 app.config.from_object('flaskconfig')
@@ -6,6 +7,10 @@ app.config.from_object('flaskconfig')
 @app.errorhandler(404)
 def not_found(error):
     return "404 error", 404
+
+@app.errorhandler(401)
+def not_found(error):
+    return "Not Authenticated", 401
 
 from app.views import general
 app.register_blueprint(general.mod)
--- app/views/general.py
+++ app/views/general.py
@@ -2,6 +2,11 @@ from flask import Blueprint
 
 mod = Blueprint('player', __name__)
 
-@mod.route('/api')
+@mod.route('/')
 def index():
     return "hello world", 200
+
+@mod.route('/signin')
+def view_of_test():
+    hiT = "sudo make me a sandwhich"
+    return "sudo make me a sandwhich"
