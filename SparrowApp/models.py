980d6c62a8c5d46cfd08fdf165de501b48f0ee85
Sergei Hanka <sergeihanka@users.noreply.github.com>
2016-05-01 23:40:08 -0500

    added UserDB, committing so Ales can use functionality

--- SparrowApp/models.py
+++ SparrowApp/models.py
@@ -8,11 +8,22 @@ from sqlalchemy.dialects.mysql import \
 		NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, \
 		TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
 
+class UserDB(db.Model):
+	email = db.Column(varchar(30), primary_key=True, nullable=False)
+	first_name = db.Column(VARCHAR(30)  NOT NULL, nullable=False)
+	last_name = db.Column(VARCHAR(30) NOT NULL, nullable=False)
+	profile_picture = db.Column(VARCHAR(200))
+	department_preference =db.Column(LONGTEXT)
+
+	def __repr__(self):
+		# return '<Project %r>' % (self.title)
+		# formats/manually creates the JSON object
+		return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class ProjectDB(db.Model):
 	projectID = db.Column(INTEGER, primary_key=True)
 	title = db.Column(VARCHAR(20), index=True)
-	description = db.Column(LONGTEXT)
+	description = db.Column(LONGTEXT, nullable=False)
 	keywords = db.Column(VARCHAR(100))
 	email = db.Column(VARCHAR(30), nullable=False)
 	time_stamp = db.Column(DATETIME, nullable=False, server_default=text('CURRENT_TIMESTAMP'))
@@ -22,30 +33,38 @@ class ProjectDB(db.Model):
 		# formats/manually creates the JSON object
 		return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+# CREATE TABLE userDB (
+# 	email varchar(30) NOT NULL PRIMARY KEY,
+# 	first_name VARCHAR(30)  NOT NULL,
+# 	last_name VARCHAR(30) NOT NULL,
+# 	profile_picture VARCHAR(200),
+# 	department_preference LONGTEXT
+# 	) ENGINE=InnoDB;
 
-# class projectDB(db.Model):
-#     projectID = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(20), index=True)
-#     description = db.Column(db.String(50))
-#     keywords = db.Column(db.String(100))
-#     email = db.Column(db.String(30))
-#     # time_stamp = db.Column(db.DateTime, nullable=False, server_default=func.now())
-#     time_stamp = db.Column(db.DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+# CREATE TABLE commentsDB (
+# 	commentID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+# 	time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+# 	projectID INT NOT NULL,
+# 	email varchar(30) NOT NULL,
+# 	comment LONGTEXT NOT NULL,
+# 	FOREIGN KEY (projectID) REFERENCES projectDB(projectID),
+# 	FOREIGN KEY (email) REFERENCES userDB(email)
+# 	) ENGINE=InnoDB;
 
+# CREATE TABLE interestDB (
+# 	email varchar(30) NOT NULL,
+# 	projectID INT NOT NULL,
+# 	UNIQUE INDEX (email,projectID),
+# 	PRIMARY KEY (email, projectID),
+# 	FOREIGN KEY (email) REFERENCES userDB(email),
+# 	FOREIGN KEY (projectID) REFERENCES projectDB(projectID)
+# 	) ENGINE=InnoDB;
 
-#     def __repr__(self):
-#         # return '<Project %r>' % (self.title)
-#         # formats/manually creates the JSON object
-		# return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+# CREATE TABLE departmentDB (
+# 	departmentID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+# 	department_name varchar(30)
+# 	) ENGINE=InnoDB;
 
-# class userDB(db.Model):
-#     email = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(20), index=True)
-#     last_name = db.Column(db.String(50))
-#     profile_picture = db.Column(db.String(100))
-#     department_preference = db.Column(db.String(30))
+# USE sergeidb;
 
-#     def __repr__(self):
-#         # return '<Project %r>' % (self.title)
-#         # formats/manually creates the JSON object
-#         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+# source populateTables.sql;
