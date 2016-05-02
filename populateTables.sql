9bddba231201ef7352570f41d4623347898a60b6
Sergei Hanka <sergeihanka@users.noreply.github.com>
2016-05-02 02:03:24 -0500

    adding department column to projectDB table

--- populateTables.sql
+++ populateTables.sql
@@ -1,17 +1,17 @@
 USE sergeidb
 
-INSERT INTO UserDB (email,first_name,last_name) VALUES ("sergeiahanka@gmail.com","Sergei","Hanka");
+INSERT INTO UserDB (email, first_name,last_name) VALUES ("sergeiahanka@gmail.com","Sergei","Hanka");
 
-INSERT INTO ProjectDB (title,description,email) VALUES ("One","testing number one","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Two","testing number two","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Three","testing number three","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Four","testing number four","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Five","testing number five","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Six","testing number six","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Seven","testing number seven","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Eight","testing number eight","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Nine","testing number nine","sergeiahanka@gmail.com");
-INSERT INTO ProjectDB (title,description,email) VALUES ("Ten","testing number ten","sergeiahanka@gmail.com");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("One","testing number one","sergeiahanka@gmail.com","Computer Science");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Two","testing number two","sergeiahanka@gmail.com","Biology");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Three","testing number three","sergeiahanka@gmail.com","Economics");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Four","testing number four","sergeiahanka@gmail.com","Nursing");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Five","testing number five","sergeiahanka@gmail.com","Economics");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Six","testing number six","sergeiahanka@gmail.com","Computer Science");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Seven","testing number seven","sergeiahanka@gmail.com","Biology");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Eight","testing number eight","sergeiahanka@gmail.com","Computer Science");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Nine","testing number nine","sergeiahanka@gmail.com","Computer Science");
+INSERT INTO ProjectDB (title,description,email, department) VALUES ("Ten","testing number ten","sergeiahanka@gmail.com","Nursing");
 
 INSERT INTO DepartmentDB (department_name) VALUES ("Computer Science");
 INSERT INTO DepartmentDB (department_name) VALUES ("Biology");
