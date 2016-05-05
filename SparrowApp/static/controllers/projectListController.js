48dd4cec14670b8d0a83237ac837609c5a473178
Evan Berg <bergev01@luther.edu>
2016-05-04 18:04:42 -0700

    added an interested switch

--- SparrowApp/static/controllers/projectListController.js
+++ SparrowApp/static/controllers/projectListController.js
@@ -24,6 +24,21 @@ app.controller('projectListCtrl', ['$rootScope',
 			});
 	    });
 
+	    angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+		.controller('SwitchDemoCtrl', function($scope) {
+		  $scope.data = {
+		    cb1: true,
+		    cb4: true,
+		    cb5: false
+		  };
+
+		  $scope.message = 'false';
+
+		  $scope.onChange = function(cbState) {
+		  	$scope.message = cbState;
+		  };
+		});
+
 	    $http({
 	      	url: '/listDepartments',
 	      	method: "GET",
