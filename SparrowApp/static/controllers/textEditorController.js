74a2e8a84f517b575497e91685a55eb410ae5788
sparrow <alevar@gmail.com>
2016-05-08 02:31:28 -0500

    added non-functional save button ti the editor

--- SparrowApp/static/controllers/textEditorController.js
+++ SparrowApp/static/controllers/textEditorController.js
@@ -17,19 +17,33 @@ app.controller('TextEditCtrl', ['$rootScope',
     $scope.tinymceModel = 'Time: ' + (new Date());
   };
 
+  tinymce.PluginManager.add('menusave', function(editor, url) {
+      editor.addMenuItem('menusave', {
+          text: 'Save',
+          context: 'file',
+          onclick: function() {
+              $('.mce-i-save').closest('button').trigger('click');
+              console.log("HELLO")
+          }
+      });
+  });
+
   $scope.tinymceOptions = {
     selector: 'textarea',
-  height: 500,
-  plugins: [
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table contextmenu paste code'
-  ],
-  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  content_css: [
-    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-    '//www.tinymce.com/css/codepen.min.css'
-  ]
+    height: 500,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table contextmenu paste save menusave'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | save',
+    content_css: [
+      '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+      '//www.tinymce.com/css/codepen.min.css'
+    ],
+    save_onsavecallback: function () { console.log('Saved'); },
+    save_oncancelcallback: function () { console.log('Save canceled'); },
+    save_enablewhendirty: true
   };
 	
 }]);
