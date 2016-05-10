63211afc5a1f83bafa1c4af013a8bddd08a4aa49
sparrow <alevar@gmail.com>
2016-05-10 16:59:23 -0500

    asdf

--- SparrowApp/flaskconfig.py
+++ SparrowApp/flaskconfig.py
@@ -3,7 +3,7 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sparrow:qwerty@radiant-earth-23997.herokuapp.com/sergeidb'
+SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://ckkqkjuuyafdke:_vraBcR3QzNTi_vB29Qh2BTTZy@ec2-54-235-65-221.compute-1.amazonaws.com:5432/dag3g9f4jatcur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 
